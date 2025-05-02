2980c468139d9f8f588347b9f88a6aec8c2f3cb1
Rhett430 <rhett430@gmail.com>
2025-05-02 13:24:58 -0400

    Added player sprite

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughput this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    game_loop(screen)
-    
-def game_loop(screen):
-    loop = True
+    clock = pygame.time.Clock()
+    dt = 0
+    Player = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
     
 if __name__ == "__main__":
