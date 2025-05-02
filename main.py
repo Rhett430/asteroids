1a42e444eac3c2d250fd29ee18adaacac7d404c9
Rhett430 <rhett430@gmail.com>
2025-05-02 13:35:03 -0400

    fixed player sprite code in main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
 from constants import *
-from circleshape import *
-from player import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    Player = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
